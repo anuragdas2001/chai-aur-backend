2f96f1617792267f6bb9a28a55c47e0f261ab0ca
Anurag Das <anuragdas2001>
2024-06-15 12:59:50 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! Yup");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai aur code</h1>");
+});
+
+app.get("/chai",(req,res)=>{
+    res.send("<h2>Chai aur code</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
